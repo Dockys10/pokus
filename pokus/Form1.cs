2b5d911e20b8d08fd188f38bd4efdafb570fb67f
Šimon Dočkal <dockals.04@spst.eu>
2022-02-09 08:36:38 +0100

    odcitani

--- pokus/Form1.cs
+++ pokus/Form1.cs
@@ -23,6 +23,8 @@ namespace pokus
             int box2 = Convert.ToInt32(textBox2.Text);
             int soucet = box1 + box2;
             label1.Text = "soucet je "+ soucet;
+            int odcitani = box1 - box2;
+            label2.Text = "odecteni je " + odcitani;
         }
     }
 }
